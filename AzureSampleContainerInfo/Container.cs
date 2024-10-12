9148f17a6b70ffb85b6dfc90b73114dd5d5f5d75
Nick Pruehs <dev@npruehs.de>
2024-10-13 00:01:12 +0200

    Store new containers in table storage.

---
+++ AzureSampleContainerInfo/Container.cs
@@ -0,0 +1,28 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace AzureSampleContainerInfo
+{
+    public class Container : ITableEntity
+    {
+        public string PartitionKey { get; set; } = "Containers";
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; } = ETag.All;
+
+        public string ContainerId { get; set; }
+
+        public string IsoCode { get; set; }
+
+        public ContainerStatus Status { get; set; }
+
+        public Container(string containerId, string isoCode, ContainerStatus status)
+        {
+            RowKey = containerId;
+
+            ContainerId = containerId;
+            IsoCode = isoCode;
+            Status = status;
+        }
+    }
+}
--- AzureSampleContainerInfo/HttpExample.cs
+++ AzureSampleContainerInfo/HttpExample.cs
@@ -1,3 +1,6 @@
+using Azure;
+using Azure.Data.Tables;
+using Azure.Identity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -15,9 +18,23 @@ namespace AzureSampleContainerInfo
         }
 
         [Function("HttpExample")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            TableServiceClient serviceClient = new("UseDevelopmentStorage=true");
+
+            TableClient client = serviceClient.GetTableClient(
+                tableName: "Containers"
+            );
+
+            Container container = new Container("TEST12345678", "22G1", ContainerStatus.Announced);
+
+            Response response = await client.UpsertEntityAsync(
+                entity: container,
+                mode: TableUpdateMode.Replace
+            );
+
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
